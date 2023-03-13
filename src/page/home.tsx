77a27af3f2dd078014c4a35e864d9d7531862b4f
aliho3en <alihossain.ahmadi@dci.education>
2023-03-13 10:09:14 +0100

    remove : slider

--- src/page/home.tsx
+++ src/page/home.tsx
@@ -1,5 +1,4 @@
 import { FC } from "react";
-import SlidShow from "../component/main/slideShow";
 import CategoryList from "../component/main/categoryList";
 import Content from "../component/main/content";
 import BrandList from "../component/main/brandList";
